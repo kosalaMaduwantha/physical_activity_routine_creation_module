102c250ea58cf5de868a1a93b2520460b0fec27d
kosalaMaduwantha <52875765+kosalaMaduwantha@users.noreply.github.com>
2023-12-01 19:29:50 +0530

    physical activity prediction implementation

--- hexagonal-flask-app/app/adapters/rest/flask_server/swagger_server/__main__.py
+++ hexagonal-flask-app/app/adapters/rest/flask_server/swagger_server/__main__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+import sys
+sys.path.append('/home/kosala/git-rep/physical_activity_routine_creation_module/hexagonal-flask-app/app/adapters/rest/flask_server/')
 import connexion
 
 from swagger_server import encoder
@@ -9,7 +10,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Diabtrek'}, pythonic_params=True)
-    app.run(port=8080)
+    app.run(port=8081)
 
 
 if __name__ == '__main__':
--- hexagonal-flask-app/app/adapters/rest/flask_server/swagger_server/controllers/phyact_pred_controller.py
+++ hexagonal-flask-app/app/adapters/rest/flask_server/swagger_server/controllers/phyact_pred_controller.py
@@ -1,12 +1,18 @@
 import connexion
 import six
-
+import sys
+sys.path.append('/home/kosala/git-rep/physical_activity_routine_creation_module/hexagonal-flask-app/')
+from app.domain.api.dtos.models import phyActData
+from app.adapters.db.mysql_adapter import MySQLAdapter
+from app.domain.services.physical_act_impl import PhysicalActImpl
 from swagger_server.models.predict_physical_act import PredictPhysicalAct  # noqa: E501
 from swagger_server.models.predict_physical_act_response_body201 import PredictPhysicalActResponseBody201  # noqa: E501
 from swagger_server.models.predict_physical_act_response_body400 import PredictPhysicalActResponseBody400  # noqa: E501
 from swagger_server.models.predict_physical_act_response_body500 import PredictPhysicalActResponseBody500  # noqa: E501
 from swagger_server import util
 
+db_sp = MySQLAdapter()
+phyActService = PhysicalActImpl(db_sp)
 
 def api_v1_physical_act_prediction_post(body):  # noqa: E501
     """predict the physical activities
@@ -19,5 +25,8 @@ def api_v1_physical_act_prediction_post(body):  # noqa: E501
     :rtype: PredictPhysicalActResponseBody201
     """
     if connexion.request.is_json:
-        body = PredictPhysicalAct.from_dict(connexion.request.get_json())  # noqa: E501
+        data = phyActData(**connexion.request.get_json())
+    
+    phyActService.predict_physical_activities(data)
+    
     return 'do some magic!'
